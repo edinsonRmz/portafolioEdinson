54d0484d1084a057b93355139ed5660d4d773419
edinsonRmz <edinsonramirez0627@gmail.com>
2023-09-07 17:34:26 -0500

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css'
 import React, { useState } from 'react'
 import MyComponet from './Components/Componet'
-import MyComonenttext from ''
+import MyComonenttext from './Components/ComponetText'
+import copyright from './Components/copyright'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
      <>
+     <MyComponet/>
+     <ComponetText/>
+     <copyright/>
+     
      </>
   )
 }
